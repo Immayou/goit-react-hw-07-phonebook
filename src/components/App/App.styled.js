10fc294595b0971781f77b93251f6df0b5edc0ad
Yuliya Zozulya <djulia@ukr.net>
2022-11-29 00:55:57 +0200

    fixing bugs

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,18 +1,15 @@
-
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-display: block;
-width: 100%;
-text-align: center;
-margin: 0 auto;
-background: linear-gradient(45deg, #EECFBA, #C5DDE8);
-`
+  display: block;
+  width: 100%;
+  text-align: center;
+  margin: 0 auto;
+  background: linear-gradient(45deg, #eecfba, #c5dde8);
+`;
 export const ContactsTitle = styled.h2`
-font-weight: 500;
-font-size: 30px;
-line-height: 1.33;
-margin-bottom: 20px;
-`
-
-
+  font-weight: 500;
+  font-size: 30px;
+  line-height: 1.33;
+  margin-bottom: 20px;
+`;
